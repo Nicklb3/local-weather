c3129b1daa06012e6c8795d74675645140825785
Nicholas Bonner <nicholaslbonner@yahoo.com>
2023-01-20 23:01:35 -0800

    finished js file

--- js/script.js
+++ js/script.js
@@ -62,6 +62,107 @@ var getForecast = function(data, cityName) {
         })
 }   
 
+var searchHistory = function(cityName) {
+    var getHistoryStorage = JSON.parse(localStorage.getItem("City History"));
+    var historyBtn = document.createElement("button");
+    if (getHistoryStorage === null) {
+      cityHistory.push(cityName);
+      historyBtn.textContent = cityName;
+      historyBtn.classList.add("w-100");
+      historyBtn.classList.add("history-btn");
+      historyBtn.setAttribute("id", "city-history");
+      historyContainer.appendChild(historyBtn);
+      localStorage.setItem("City History", JSON.stringify(cityHistory));
+    }
+    else {
+      cityHistory = getHistoryStorage;
+      if (cityHistory.includes(cityName)) {
+        return null;
+      }
+      else {
+        historyBtn.textContent = cityName;
+        historyBtn.classList.add("w-100");
+        historyBtn.classList.add("history-btn");
+        historyBtn.setAttribute("id", "city-history");
+        historyContainer.appendChild(historyBtn);
+        cityHistory.push(cityName);
+        localStorage.setItem("City History", JSON.stringify(cityHistory));
+      }
+    }       
+}
+
+var getSearchHistory = function() {
+    var getHistoryStorage = JSON.parse(localStorage.getItem("City History"));
+    if (getHistoryStorage === null) {
+      return;
+    }
+    else {
+      cityHistory = getHistoryStorage;
+      for (var i = 0; i < cityHistory.length; i++) {
+      var historyBtn = document.createElement("button");
+      historyBtn.textContent = cityHistory[i];
+      historyBtn.classList.add("w-100");
+      historyBtn.classList.add("history-btn");
+      historyBtn.setAttribute("id", "city-history");
+      historyContainer.appendChild(historyBtn);
+      }
+    }
+}
+
+var recallHistory = function(event) {
+    var cityHistoryText = event.target.textContent;
+    dayIndex = 1;
+    getLatLong(cityHistoryText);
+}
+
+var displayForecast = function(weatherData, cityName) {
+    var cityNameEl = document.querySelector("#city-name");
+    var tempEl = document.querySelector("#temp");
+    var windEl = document.querySelector("#wind");
+    var humidityEL = document.querySelector("#humidity");
+    var uvEl = document.querySelector("#uv-index");
+    var iconImg = document.querySelector("#icon-img");
+    if (weatherData.status === "city not found") {
+      console.log("Nothing");
+    }
+    else {
+      iconImg.src = `http://openweathermap.org/img/wn/${weatherData.current.weather[0].icon}.png`
+      cityNameEl.textContent = `${cityName} (${currentDate})`;
+      tempEl.textContent = weatherData.current.temp
+      windEl.textContent = weatherData.current.wind_speed
+      humidityEL.textContent = weatherData.current.humidity
+      uvEl.textContent = weatherData.current.uvi
+      var uvIndex = weatherData.current.uvi
+      if (uvIndex <= 2) {
+        uvEl.className = "low"
+      } 
+      else if (3 <= uvIndex <= 7) {
+        uvEl.className = "mid"
+      }
+      else if (uvIndex >= 8) {
+        uvEl.className = "high"
+      }
+      var i = 0;
+      $(".forecast-box").each(function() {
+        var futureDay = moment().add(dayIndex, "days").format("MM/DD/YYYY");
+        var futureDate = $(this).find("span")[0];
+        var futureTemp = $(this).find("span")[1];
+        var futureWind = $(this).find("span")[2];
+        var futureHumidity = $(this).find("span")[3];
+        var futureIcon = $(this).find("img")[0];
+        futureDate.textContent = futureDay;
+        futureIcon.src = `http://openweathermap.org/img/wn/${weatherData.daily[i].weather[0].icon}.png`
+        futureTemp.textContent = weatherData.daily[i].temp.day;
+        futureWind.textContent = weatherData.daily[i].wind_speed;
+        futureHumidity.textContent = weatherData.daily[i].humidity;
+        i++;
+        dayIndex++;
+      })
+    }
+}
+
+
+
 
 
 
